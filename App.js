6c55731dc017e428fd62693d7a3e7b3a15e01115
Piyush Thakur <piyushthakur3769@gmail.com>
2021-10-13 19:17:26 +0530

    Used Views, Text and Styles

--- App.js
+++ App.js
@@ -5,7 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello friends, Ashtami Mubarak!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Auspicious<Text style={styles.boldText}> Night!</Text></Text>
+        <Text>Enjoy!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +23,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
   },
 });
