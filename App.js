3e093e543b4cc82a31d9055b3991f3ed95e21641
Piyush Thakur <piyushthakur3769@gmail.com>
2021-10-14 00:34:46 +0530

    Text Input

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Piyush');
+  const [age, setAge] = useState('22');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter name:</Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. Piyush Thakur'
+        onChangeText={(value) => setName(value)}/>
+      <Text>Enter age:</Text>
+      <TextInput 
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g. 22'
+        onChangeText={(value) => setAge(value)}/>
+      <Text>name: {name}, age: {age}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +34,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200, 
+  }
 });
